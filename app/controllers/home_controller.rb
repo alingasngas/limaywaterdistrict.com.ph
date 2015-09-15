8e65d2a25437f1886b8d91416b320006f0f700fe
Louie Cotejar <lchavoc214@yahoo.com>
2015-09-15 12:25:34 +0800

    changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,7 @@ class HomeController < ApplicationController
   def index
     @banners = Banner.where('featured = ? AND activate = ?', true, true)
     @page = Page.where(is_root: true).first
+    @archives = Post.active.order('publish_date DESC').group_by {|p| p.publish_date.beginning_of_month rescue p.created_at.beginning_of_month }
     @meta = { title: 'Limay Water District', description: @page.meta_description ? @page.meta_description : '', url: root_url, image:  asset_url('assets/mariwad_office.jpg') }
     #render json: @meta#_description
   end
